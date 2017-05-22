c3779cbcf0f40fec15fc80304e81fb2a2127df3a
jaebradley <jae.b.bradley@gmail.com>
2017-05-22 19:28:37 -0400

    add status family to index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 import HttpStatusIdentifier from './services/HttpStatusIdentifier';
 import HttpStatus from './data/HttpStatus';
+import HttpStatusFamily from './data/HttpStatusFamily';
 
 module.exports = {
   HttpStatusIdentifier,
   HttpStatus,
+  HttpStatusFamily,
 };
