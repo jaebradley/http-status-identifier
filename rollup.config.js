70e14921c6ba5b13d019a67bf5c736b2550f6537
jaebradley <jae.b.bradley@gmail.com>
2018-05-16 17:04:20 -0400

    build(v2): upgrade packages for rollup migration
    BREAKING CHANGE: use rollup to build package

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import localResolve from 'rollup-plugin-local-resolve';
+import filesize from 'rollup-plugin-filesize';
+import minify from 'rollup-plugin-babel-minify';
+import uglify from 'rollup-plugin-uglify';
+
+import pkg from './package.json';
+
+const config = {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'umd',
+      name: pkg.name,
+    },
+    {
+      file: 'build/index.cjs.js',
+      format: 'cjs',
+      name: pkg.name,
+    },
+    {
+      file: 'build/index.esm.js',
+      format: 'es',
+      name: pkg.name,
+    },
+  ],
+  plugins: [
+    babel({ exclude: 'node_modules/**' }),
+    localResolve(),
+    resolve({
+      module: true,
+      jsnext: true,
+      main: true,
+      preferBuiltins: true,
+      browser: true,
+      modulesOnly: true,
+    }),
+    minify(),
+    uglify(),
+    commonjs(),
+    filesize(),
+  ],
+};
+
+export default config;
